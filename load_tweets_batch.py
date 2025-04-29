ae8a0ac8155d20aa3d0e8fa8207af3edcc653e8a
stell25 <scheng25@students.claremontmckenna.edu>
2025-04-28 17:35:12 -0700

    another commit

--- load_tweets_batch.py
+++ load_tweets_batch.py
@@ -391,7 +391,7 @@ def _insert_tweets(connection,input_tweets):
     ######################################## 
     # STEP 2: perform the actual SQL inserts
     ######################################## 
-    with connection.begin() as trans:
+    #with connection.begin() as trans:
 
         # use the bulk_insert function to insert most of the data
         bulk_insert(connection, 'users', users)
--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,14 +6,15 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
+    echo
     unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:55555 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
-#    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:55556 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
+    echo
     python3 load_tweets.py --db postgresql://postgres:pass@localhost:55556/postgres --inputs "$file" 
 done
 
@@ -23,3 +24,4 @@ echo '==========================================================================
 time for file in $files; do
     python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:55557/ --inputs $file
 done
+
